8338ebe61c9b130f275413519c2733337735acad
Thabang <whitemodise@gmail.com>
2024-06-30 19:38:49 +0200

    Added user input functionality

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,5 @@
 # message to be printed
 # message = "Hello World!"
-message = "Git is awesome!"
+# message = "Git is awesome!"
+message = input("HI, what message would you like to print?\t")
 print(message)
